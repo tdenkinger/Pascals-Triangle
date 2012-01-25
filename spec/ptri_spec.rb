cd93f62254b802d4d9e4c6d78c9168f414908a2e
Troy Denkinger <tdenkinger@gmail.com>
2012-01-24 23:20:59 -0500

    Added a test for odd number rows.  Also tweaked the tests to state what the code does rather than what it 'should' do.

--- spec/ptri_spec.rb
+++ spec/ptri_spec.rb
@@ -1,30 +1,38 @@
 describe "PTRI" do
-  it "should return a single item array for row 1" do
-    PTRI.row(1).should have(1).items
+  context "with small values" do
+    it "returns a single item array for row 1" do
+      PTRI.row(1).should have(1).items
+    end
+
+    it "returns an array of [1] for row 1" do 
+      PTRI.row(1).should =~ [1]
+    end
+
+    it "returns an array of [1,1] for row 2" do
+      PTRI.row(2).should =~ [1,1]
+    end
+    
+    it "returns an array of [1,2,1] for row 3" do
+      PTRI.row(3).should =~ [1,2,1]
+    end
+
+    it "returns an array of [1,3,3,1] for row 4" do
+      PTRI.row(4).should =~ [1,3,3,1]
+    end
+
+    it "returns an array of [1,4,6,4,1] for row 5" do
+      PTRI.row(5).should =~ [1,4,6,4,1]
+    end
   end
 
-  it "should return an array of [1] for row 1" do 
-    PTRI.row(1).should =~ [1]
-  end
+  context "with large values" do
+    it "returns an array of 50 elements for row 50" do
+      PTRI.row(50).count.should == 50 
+    end
 
-  it "should return an array of [1,1] for row 2" do
-    PTRI.row(2).should =~ [1,1]
-  end
-  
-  it "should return an array of [1,2,1] for row 3" do
-    PTRI.row(3).should =~ [1,2,1]
-  end
-
-  it "should return an array of [1,3,3,1] for row 4" do
-    PTRI.row(4).should =~ [1,3,3,1]
-  end
-
-  it "should return an array of [1,4,6,4,1] for row 5" do
-    PTRI.row(5).should =~ [1,4,6,4,1]
-  end
-
-  it "should have 50 elements for row 50" do
-    PTRI.row(50).count.should == 50 
+    it "returns and array of 71 elements for row 71" do
+      PTRI.row(71).count.should == 71
+    end
   end
 end
 
